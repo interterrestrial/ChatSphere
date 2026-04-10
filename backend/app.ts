f0a8d5ff75009769d320e4ff20670e79231bb116
Deepak <Deepak@Deepaks-MacBook-Pro.local>
2026-04-10 12:57:42 +0530

    Frontend Done, Diagrams done, Authentication Done

--- backend/app.ts
+++ backend/app.ts
@@ -2,8 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
-import chatRoutes from "./routes/chat.routes";
-import userRoutes from "./routes/user.routes";
+
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
@@ -13,9 +12,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,6 @@ dotenv.config();
 import http from 'http';
 import app from './app';
 import connectDB from './db/db';
-import { initSocket } from './utils/socket';
 
 const port: number = Number(process.env.PORT || 6001);
 
@@ -12,8 +11,6 @@ connectDB();
 
 const server = http.createServer(app);
 
-// Initialize Socket.io
-initSocket(server);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
